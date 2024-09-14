2b8d3d85d9f09c190a4556aa7140bbc8cf0c771d
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-09-14 18:56:23 +0700

    dom first commit

---
+++ js/dom.js
@@ -0,0 +1,9 @@
+const licollaction = document.getElementsByTagName('li');
+for(const li of licollaction){
+    console.log(li.innerText);
+}
+
+const headingTag = document.getElementsByTagName('h1');
+for(const h of headingTag){
+    console.log(h);
+}
