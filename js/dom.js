dd5f9d2e7ae5ac698b29844b0337bff550440cbb
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-09-14 19:23:39 +0700

    get tag cls

--- js/dom.js
+++ js/dom.js
@@ -1,9 +1,20 @@
-const licollaction = document.getElementsByTagName('li');
-for(const li of licollaction){
-    console.log(li.innerText);
-}
+// const licollaction = document.getElementsByTagName('li');
+// for(const li of licollaction){
+//     console.log(li.innerText);
+// }
 
-const headingTag = document.getElementsByTagName('h1');
-for(const h of headingTag){
-    console.log(h);
+// const headingTag = document.getElementsByTagName('h1');
+// for(const h of headingTag){
+//     console.log(h);
+// }
+
+
+const title = document.getElementById('product-titel');
+title.innerHTML = 'change by js'
+console.log(title);
+
+const productItems = document.getElementsByClassName('style-items')
+console.log(productItems);
+for(const product of productItems){
+    console.log(product.innerText);
 }
