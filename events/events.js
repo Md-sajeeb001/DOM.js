6345b9f144f9de4b7e2085748b5ab11f86b8e9fb
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-09-15 19:51:01 +0700

    addEventListener

--- events/events.js
+++ events/events.js
@@ -15,3 +15,26 @@ const makeBtn = document.getElementById('makeBlue');
 makeBtn.onclick = function make(){
     document.body.style.fontSize = '2rem';
 }
+
+// addEventListner 
+
+// option one
+const btnOne = document.getElementById('btn1');
+btnOne.addEventListener('click', btn)
+
+function btn(){
+    document.body.style.backgroundColor = 'pink';
+}
+
+// option two 
+
+document.getElementById('btn2').addEventListener('click', function(){
+    document.body.style.color = 'red';
+    document.body.style.backgroundColor = 'goldenrod'
+})
+
+// option three
+
+function btn3(){
+    document.body.style.backgroundColor = 'Purple'
+}
